4aa52f0517306b448740c461c7b3cb9362aa771e
Eduardo <jose.eduardo.rodriguez.antunez@gmail.com>
2023-11-21 13:25:10 -0600

    Show product view to user.

--- src/main/java/com/miuma/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/miuma/ecommerce/springecommerce/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.miuma.ecommerce.springecommerce.controller;
 
 import com.miuma.ecommerce.springecommerce.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductService productService;
@@ -19,6 +24,12 @@ public class HomeController {
         model.addAttribute("products", productService.findAll());
 
         return "user/home";
+    }
+
+    @GetMapping("homeproduct/{id}")
+    public String homeProduct(@PathVariable Integer id){
+        log.info("product Id sent like parameter {}", id);
+        return "user/homeproduct";
     }
 
 }
