dbf09d56af7bf1e0f69871e920c7bc434d0f5c38
Eduardo <jose.eduardo.rodriguez.antunez@gmail.com>
2023-11-21 13:54:15 -0600

    Creation of shopping cart.

--- src/main/java/com/miuma/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/miuma/ecommerce/springecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -39,6 +40,11 @@ public class HomeController {
         model.addAttribute("product", product);
 
         return "user/homeproduct";
+    }
+
+    @PostMapping("/cart")
+    public String addCart(){
+        return "user/cart";
     }
 
 }
