bc15f98b2b093b2c3c7b0d1d24626918ec5b08ae
Eduardo <jose.eduardo.rodriguez.antunez@gmail.com>
2024-01-26 15:32:24 -0600

    Show the shopping view to user.

--- src/main/java/com/miuma/ecommerce/springecommerce/controller/UserController.java
+++ src/main/java/com/miuma/ecommerce/springecommerce/controller/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,6 +62,12 @@ public class UserController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/shopping")
+    public String getShopping(Model model, HttpSession session){
+        model.addAttribute("session", session.getAttribute("iduser"));
+        return "user/shopping";
     }
 
 }
